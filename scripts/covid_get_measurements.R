6032e620eb98197c78773b006e373a1c288acddb
hubert.thieriot <ht@erprofiler.com>
2020-04-15 15:27:19 +0800

    cleaned

--- scripts/covid_get_measurements.R
+++ scripts/covid_get_measurements.R
@@ -1,6 +1,6 @@
 #----------------------------------------------------------
-# This script merges covid data from coronadatascraper.com
-# with CREA Air Quality data available in the vicinity.
+# This script merges covid data (exc. China) from coronadatascraper.com
+# with CREADB Air Quality data available in the vicinity.
 #----------------------------------------------------------
 require(creadb)
 require(sf)
@@ -17,11 +17,11 @@ poll <- c(creadb::PM25)
 # Execution
 #-----------------
 # Get COVID timeseries and locations of interest
-cor_csv <- read.csv(url('https://coronadatascraper.com/timeseries.csv'))
-cor_sf <- st_as_sf(cor_csv %>%
-                     distinct(name, country, lat, long) %>%
-                     filter(!is.na(lat)) %>%
-                     filter(country != 'China'), coords = c("long", "lat"), crs=4326)
+cor_df <- read.csv(url('https://coronadatascraper.com/timeseries.csv'))
+cor_sf <- st_as_sf(cor_df %>%
+                     dplyr::distinct(name, country, lat, long) %>%
+                     dplyr::filter(!is.na(lat)) %>%
+                     dplyr::filter(country != 'China'), coords = c("long", "lat"), crs=4326)
 
 cor_buf_sf <- cor_sf %>%
   st_transform(crs=3857) %>%
@@ -30,7 +30,9 @@ cor_buf_sf <- cor_sf %>%
 # Find Air Quality locations close to it
 locs <- creadb::locations()
 locs_sf <- st_as_sf(locs, crs=4326)
-cor_buf_joined <- st_join(cor_buf_sf, locs_sf %>% st_transform(crs = 3857), join=st_contains, suffix=c(".cov",".aq"))
+cor_buf_joined <- st_join(cor_buf_sf, locs_sf %>% st_transform(crs = 3857),
+                          join=st_contains,
+                          suffix=c(".cov",".aq"))
 loc_ids <- setdiff(unique(cor_buf_joined$id),NA)
 
 # Get air measurements
@@ -40,16 +42,20 @@ meas <- creadb::measurements(location_id=loc_ids,
                              poll=poll)
 
 # Convert measurements date to local date string to match covid data
-meas <- meas %>% rowwise() %>% mutate(
-  date =strftime(date, format="%Y-%m-%d", tz=ifelse(!is.na(timezone), timezone, 'UTC'))
-)
+meas <- meas %>%
+  rowwise() %>%
+  dplyr::mutate(date=strftime(date, format="%Y-%m-%d", tz=ifelse(!is.na(timezone), timezone, 'UTC')))
 
 # Join back
 # 1. add covid location name to measurements
-cor_buf_joined <- cor_buf_joined %>% select(name.cov, city) %>% mutate(city=tolower(city)) %>%
-  right_join(meas %>% select(date, city, poll, unit, timezone, value, source), by=c('city'))
+cor_buf_joined <- cor_buf_joined %>%
+  dplyr::select(name.cov, city) %>%
+  dplyr::mutate(city=tolower(city)) %>%
+  dplyr::right_join(meas %>% dplyr::select(date, city, poll, unit, timezone, value, source), by=c('city'))
 
 # 2. merge with original dataset
-cor_csv_w_aq <- cor_csv %>% left_join(cor_buf_joined %>% select(name.cov, date, poll, unit, value, source), on=c("name"="name.cov", "date"="date"))
-
-
+# We do inner join to only keep measurements with both cov and aq data.
+# Change to left_join if you want to keep the whole dataset
+cor_aq_df <- cor_df %>%
+  dplyr::inner_join(cor_buf_joined %>% dplyr::select(name.cov, date, poll, unit, value, source),
+                   on=c("name"="name.cov", "date"="date"))
