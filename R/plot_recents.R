217900a7bccbace7a508ba508ba708608454a0ff
hubert.thieriot <ht@erprofiler.com>
2020-05-17 11:20:22 +0800

    -adding update date

--- R/plot_recents.R
+++ R/plot_recents.R
@@ -30,7 +30,7 @@ plot_recents <- function(folder, source, countries=NULL, polls=NULL){
             labs(
               title=paste("Air pollutant concentrations in",country_name),
               subtitle=ifelse(isnull(running),NULL,paste0(running,"-day running average")),
-              caption=paste("Source: CREA based on ", sources[[source]],"."))
+              caption=paste("Source: CREA based on ", sources[[source]],". Updated on",format(Sys.Date(), format="%d %B %Y")))
         )
 
         for(size in names(width)){
