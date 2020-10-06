e41166bb2f6427184a35e40d5c346e05945da5cf
Hubert Thieriot <hubert@energyandcleanair.org>
2020-10-06 19:53:51 +0800

    -removing old tabs

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -67,70 +67,70 @@ ui <- fluidPage(
                 )
             )
         ),
-
-        tabPanel("Exceedances", fluid = TRUE,
-             sidebarLayout(
-                 sidebarPanel(
-                     width = 2,
-                     selectInput("exc_country",
-                                 "Country:",
-                                 choices = unique(locations$country),
-                                 multiple=T,
-                                 selected = "IN"
-                     ),
-                     uiOutput("selectInputExcCity"),
-
-
-                     sliderInput("exc_year",
-                                 "Year:",
-                                 min=2015, max=2020, value=2020, sep="", step=1, ticks = F
-                     ),
-
-                     pickerInput("exc_status",
-                                 "Status",
-                                 choices = exc_status_labels,
-                                 options = list(`actions-box` = TRUE,
-                                                `selected-text-format` = "count > 3"),
-                                 multiple = T,
-                                 selected = exc_status_labels),
-
-                     pickerInput("exc_poll",
-                                 "Pollutant",
-                                 choices = polls,
-                                 options = list(`actions-box` = TRUE,
-                                                `selected-text-format` = "count > 3"),
-                                 multiple = T,
-                                 selected = polls),
-
-                     pickerInput("exc_aggregation_period",
-                                 "Aggregation period:",
-                                 choices = unique(standards$aggregation_period),
-                                 options = list(`actions-box` = TRUE,
-                                                `selected-text-format` = "count > 3"),
-                                 multiple = T,
-                                 selected = unique(standards$aggregation_period)
-                     ),
-
-                     pickerInput("exc_standard_org",
-                                 "Standard source:",
-                                 choices = unique(standards$organization),
-                                 multiple = T,
-                                 options = list(`actions-box` = TRUE),
-                                 selected = c("EU","WHO","NAAQS")
-                     ),
-                     downloadButton("exc_download_csv", "Download (.csv)"),
-                     downloadButton("exc_download_rds", "Download (.rds)")
-
-                 ),
-
-                 mainPanel(
-                     width=10,
-                    # plotOutput("exc_status_map"),
-                    # DT::dataTableOutput("exc_status_table")
-                    DT::dataTableOutput("exc_table")
-                 )
-            )
-        ),
+#
+#         tabPanel("Exceedances", fluid = TRUE,
+#              sidebarLayout(
+#                  sidebarPanel(
+#                      width = 2,
+#                      selectInput("exc_country",
+#                                  "Country:",
+#                                  choices = unique(locations$country),
+#                                  multiple=T,
+#                                  selected = "IN"
+#                      ),
+#                      uiOutput("selectInputExcCity"),
+#
+#
+#                      sliderInput("exc_year",
+#                                  "Year:",
+#                                  min=2015, max=2020, value=2020, sep="", step=1, ticks = F
+#                      ),
+#
+#                      pickerInput("exc_status",
+#                                  "Status",
+#                                  choices = exc_status_labels,
+#                                  options = list(`actions-box` = TRUE,
+#                                                 `selected-text-format` = "count > 3"),
+#                                  multiple = T,
+#                                  selected = exc_status_labels),
+#
+#                      pickerInput("exc_poll",
+#                                  "Pollutant",
+#                                  choices = polls,
+#                                  options = list(`actions-box` = TRUE,
+#                                                 `selected-text-format` = "count > 3"),
+#                                  multiple = T,
+#                                  selected = polls),
+#
+#                      pickerInput("exc_aggregation_period",
+#                                  "Aggregation period:",
+#                                  choices = unique(standards$aggregation_period),
+#                                  options = list(`actions-box` = TRUE,
+#                                                 `selected-text-format` = "count > 3"),
+#                                  multiple = T,
+#                                  selected = unique(standards$aggregation_period)
+#                      ),
+#
+#                      pickerInput("exc_standard_org",
+#                                  "Standard source:",
+#                                  choices = unique(standards$organization),
+#                                  multiple = T,
+#                                  options = list(`actions-box` = TRUE),
+#                                  selected = c("EU","WHO","NAAQS")
+#                      ),
+#                      downloadButton("exc_download_csv", "Download (.csv)"),
+#                      downloadButton("exc_download_rds", "Download (.rds)")
+#
+#                  ),
+#
+#                  mainPanel(
+#                      width=10,
+#                     # plotOutput("exc_status_map"),
+#                     # DT::dataTableOutput("exc_status_table")
+#                     DT::dataTableOutput("exc_table")
+#                  )
+#             )
+#         ),
 
         tabPanel("Trajectories", fluid = TRUE,
                  sidebarLayout(
@@ -156,49 +156,49 @@ ui <- fluidPage(
                          # DT::dataTableOutput("trajs_table")
                      )
                  )
-        ),
-
-        tabPanel("Download", fluid = TRUE,
-                 sidebarLayout(
-                     sidebarPanel(
-                         width = 3,
-                         selectInput("down_country",
-                                     "Country:",
-                                     choices = unique(locations$country),
-                                     multiple=T,
-                                     selected = "IN"
-                         ),
-                         uiOutput("selectInputDownCity"),
-                         pickerInput("down_poll",
-                                     "Pollutant",
-                                     choices = polls,
-                                     options = list(`actions-box` = TRUE,
-                                                    `selected-text-format` = "count > 3"),
-                                     multiple = T,
-                                     selected = polls),
-                         sliderInput("down_years",
-                                     "Year:",
-                                     min=2015, max=2020, value=c(2018, 2020), sep="", step=1, ticks = F
-                         ),
-                         selectInput("down_averaging",
-                                     "Time averaging:",
-                                     choices = averagings,
-                                     selected = "day"
-                         ),
-                         actionButton("down_refresh", "Refresh Measurements!")
-
-                     ),
-
-                     mainPanel(
-                         width=9,
-                         # plotOutput("exc_status_map"),
-                         # DT::dataTableOutput("exc_status_table")
-                         DT::dataTableOutput("down_table") %>% withSpinner(color="#0dc5c1"),
-                         downloadButton("down_download_csv", "Download (.csv)"),
-                         downloadButton("down_download_rds", "Download (.rds)")
-                     )
-                 )
         )
+#
+#         tabPanel("Download", fluid = TRUE,
+#                  sidebarLayout(
+#                      sidebarPanel(
+#                          width = 3,
+#                          selectInput("down_country",
+#                                      "Country:",
+#                                      choices = unique(locations$country),
+#                                      multiple=T,
+#                                      selected = "IN"
+#                          ),
+#                          uiOutput("selectInputDownCity"),
+#                          pickerInput("down_poll",
+#                                      "Pollutant",
+#                                      choices = polls,
+#                                      options = list(`actions-box` = TRUE,
+#                                                     `selected-text-format` = "count > 3"),
+#                                      multiple = T,
+#                                      selected = polls),
+#                          sliderInput("down_years",
+#                                      "Year:",
+#                                      min=2015, max=2020, value=c(2018, 2020), sep="", step=1, ticks = F
+#                          ),
+#                          selectInput("down_averaging",
+#                                      "Time averaging:",
+#                                      choices = averagings,
+#                                      selected = "day"
+#                          ),
+#                          actionButton("down_refresh", "Refresh Measurements!")
+#
+#                      ),
+#
+#                      mainPanel(
+#                          width=9,
+#                          # plotOutput("exc_status_map"),
+#                          # DT::dataTableOutput("exc_status_table")
+#                          DT::dataTableOutput("down_table") %>% withSpinner(color="#0dc5c1"),
+#                          downloadButton("down_download_csv", "Download (.csv)"),
+#                          downloadButton("down_download_rds", "Download (.rds)")
+#                      )
+#                  )
+#         )
     )
 )
 
