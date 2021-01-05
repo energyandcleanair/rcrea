9b4b7ad87f6f7fc963c08b8426c9c5ede7f2a49c
Hubert Thieriot <hubert@energyandcleanair.org>
2021-01-05 12:28:31 +0000

    -updating violations

--- R/violations.R
+++ R/violations.R
@@ -9,48 +9,69 @@ violations.guidelines <- function(){
 violations <- function(source, city,
                        date_from, date_to,
                        frequency=c("hour","day","year"),
+                       level="city",
                        country=NULL,
                        location_type=NULL,
                        poll=NULL,
                        orgs=c("EU", "WHO")){
 
+  l <- rcrea::locations(source=source,
+                        country=country,
+                        city=city,
+                        level=level,
+                        with_metadata = T)
+
   m.hour <- rcrea::measurements(source=source,
                                 country=country,
+                                aggregate_level = level,
                                 city=city,
                                 location_type=location_type,
                                 average_by = "hour",
                                 poll=poll,
                                 date_from=date_from,
                                 date_to=date_to) %>%
-    mutate(frequency="1-hour")
+    mutate(frequency="1-hour",
+           aggregation_function="mean")
+
+  m.8hour <- rcrea::utils.running_average(m.hour,
+                                       average_width = 8,
+                                       average_by = "hour") %>%
+    mutate(frequency="8-hour",
+           aggregation_function="mean")
 
   m.day <- rcrea::measurements(source=source,
                                 country=country,
                                 city=city,
+                                aggregate_level = level,
                                 location_type=location_type,
                                 average_by = "day",
                                 poll=poll,
                                 date_from=date_from,
                                 date_to=date_to) %>%
-    mutate(frequency="24-hour")
+    mutate(frequency="24-hour",
+           aggregation_function="mean")
 
   m.year <- m.day %>%
     mutate(date=lubridate::floor_date(date, unit='year')) %>%
     group_by_at(setdiff(names(.), "date")) %>%
     summarise(value=mean(value, na.rm=T)) %>%
     ungroup() %>%
-    mutate(frequency="year")
+    mutate(frequency="year",
+           aggregation_function="mean")
 
 
   guidelines <- violations.guidelines()
 
-  m <- bind_rows(m.hour, m.day, m.year) %>%
+  m <- bind_rows(m.hour, m.8hour, m.day, m.year) %>%
+    filter(!is.na(date)) %>%
     dplyr::inner_join(guidelines %>%
                 select(poll=pollutant,
+                       aggregation_function,
                        threshold, unit, exceedance_allowed_per_year,
                        frequency=aggregation_period,
                        aggregation_function,
-                       organization, standard_id))
+                       organization, standard_id),
+                by=c("poll","unit","frequency","aggregation_function"))
 
   count_violations <- function(group){
     group %>%
@@ -58,11 +79,14 @@ violations <- function(source, city,
       nrow()
   }
 
-  m %>%
+  m.violations <- m %>%
     group_by(location_id, process_id, poll, unit, source,
              year=lubridate::year(date), frequency,
              standard_id, exceedance_allowed_per_year) %>%
-    summarise(n_violations=count_violations(dplyr::cur_data()))
+    summarise(n_violations=count_violations(dplyr::cur_data())) %>%
+    ungroup()
+
+  m.violations %>% left_join(l)
 
 }
 
