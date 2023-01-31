3372d2db455fc5e22d53b101171330a3c0afb63c
StefanosPapaiordanidis <spapaiord@gmail.com>
2023-01-31 15:31:01 +0200

    Created a gee.init() function, fixed gee.get_lc, included and tested input options for coord list, bbox, sf, and gee polygon in gee.get_data

--- R/gee.R
+++ R/gee.R
@@ -1,88 +1,80 @@
-require(raster)
-require(terra)
-library(rgee)
-library(ncdf4)
-library(lubridate)
-library(glue)
+gee.init <- function(){
+  ee_check()
+  ee_Initialize(user = 'ndef', drive = TRUE)
+}
 
-
-ee_check()
-ee_Initialize()
-ee_Initialize(user = 'ndef', drive = TRUE)
-
-gee_get.srtm <- function(){
+gee.get_srtm <- function(){
   srtm <- ee$Image('CGIAR/SRTM90_V4') %>%
-    ee$Image$toFloat() %>%
-    ee$Image$rename('Elevation')
+          ee$Image$toFloat() %>%
+          ee$Image$rename('Elevation')
   return(srtm)
 }
 
-gee_get.pop <- function(){
+gee.get_pop <- function(){
   pop <- ee$ImageCollection("CIESIN/GPWv411/GPW_Population_Density") %>%
-    ee$ImageCollection$sort('system:time_start', F) %>%
-    ee$ImageCollection$first() %>%
-    ee$Image$toFloat() %>%
-    ee$Image$rename('Population Density')
+          ee$ImageCollection$sort('system:time_start', F) %>%
+          ee$ImageCollection$first() %>%
+          ee$Image$toFloat() %>%
+          ee$Image$rename('Population Density')
   return(pop)
 }
 
-gee_get.lat <- function(){
+gee.get_lat <- function(){
   lat <- ee$Image$pixelLonLat()$select('latitude') %>%
-    ee$Image$toFloat() %>%
-    ee$Image$rename('Latitude')
+          ee$Image$toFloat() %>%
+          ee$Image$rename('Latitude')
   return(lat)
 }
 
-gee_get.lon <- function(){
+gee.get_lon <- function(){
   lon <- ee$Image$pixelLonLat()$select('longitude') %>%
-    ee$Image$toFloat() %>%
-    ee$Image$rename('Longitude')
+          ee$Image$toFloat() %>%
+          ee$Image$rename('Longitude')
   return(lon)
 }
 
-gee_get.lc <- function(date_from, date_to){
+gee.get_lc <- function(date_from, date_to){
   lc <- ee$ImageCollection("GOOGLE/DYNAMICWORLD/V1") %>%
-    ee$ImageCollection$filterDate(date_from, date_to) %>%
-    ee$ImageCollection$select('label') %>%
-    ee$ImageCollection$mean() %>%
-    ee$Image$reproject(ee$Projection('EPSG:4326')) %>%
-    ee$Image$toFloat() %>%
-    ee$Image$rename('Land Cover')
+        ee$ImageCollection$filterDate(date_from, date_to) %>%
+        ee$ImageCollection$select('label') %>%
+        ee$ImageCollection$mosaic() %>%
+        ee$Image$toFloat() %>%
+        ee$Image$rename('Land Cover')
   return(lc)
 }
 
-gee_get.dist_coast <- function(max_dist=4000000){
+gee.get_distcoast <- function(max_dist=4000000){
   water_mask <- ee$Image('MODIS/MOD44W/MOD44W_005_2000_02_24') %>%
-    ee$Image$select('water_mask')
+                ee$Image$select('water_mask')
 
   euclidean_kernel <- ee$Kernel$euclidean(max_dist, 'meters')
   euclidean_dist_um <- water_mask$distance(euclidean_kernel)
   dist_coast <- euclidean_dist_um$updateMask(water_mask$Not())%>%
-    ee$Image$reproject(ee$Projection('EPSG:4326')) %>%
-    ee$Image$toFloat() %>%
-    ee$Image$rename('Distance to Coast')
+                ee$Image$reproject(ee$Projection('EPSG:4326')) %>%
+                ee$Image$toFloat() %>%
+                ee$Image$rename('Distance to Coast')
   return(dist_coast)
 }
 
-gee_get.dist_urban <- function(max_dist=400000){
+gee.get_disturban <- function(max_dist=400000){
   urban_mask <- ee$Image('DLR/WSF/WSF2015/v1') %>%
-    ee$Image$unmask()
+                ee$Image$unmask()
 
   euclidean_kernel <- ee$Kernel$euclidean(max_dist, 'meters')
   dist_urban <- urban_mask$distance(euclidean_kernel) %>%
-    ee$Image$toFloat() %>%
-    ee$Image$rename('Distance to Urban')
+                  ee$Image$toFloat() %>%
+                  ee$Image$rename('Distance to Urban')
   return(dist_urban)
 }
 
-gee_get.pm25 <- function(){
+gee.get_pm25 <- function(){
   pm25 <- ee$Image('projects/quickstart-1595845282060/assets/pm25') %>%
-    ee$Image$toFloat() %>%
-    ee$Image$rename('PM25')
+          ee$Image$toFloat() %>%
+          ee$Image$rename('PM25')
   return(pm25)
 }
 
-gee_get.o3 <- function(date_from, date_to){
+gee.get_o3 <- function(date_from, date_to){
   date_from_dt <- ymd(date_from,tz='UTC')
   date_to_dt <- ymd(date_to,tz='UTC')
 
@@ -104,7 +96,7 @@ gee_get.o3 <- function(date_from, date_to){
   return(o3)
 }
 
-gee_get.no2 <- function(date_from, date_to){
+gee.get_no2 <- function(date_from, date_to){
   date_from_dt <- ymd(date_from,tz='UTC')
   date_to_dt <- ymd(date_to,tz='UTC')
 
@@ -126,41 +118,59 @@ gee_get.no2 <- function(date_from, date_to){
   return(no2)
 }
 
-gee_get.data <- function(date_from,
+gee.get_data <- function(date_from,
                          date_to,
-                         roi=NULL,
-                         res=10000){
+                         gee_poly=NULL,
+                         sf_obj=NULL,
+                         bbox_obj=NULL,
+                         coords_vector_list=NULL,
+                         res=50000){
+  # The res argument should not be over 78000
 
   if (!dir.exists('results')){
     dir.create('results')
   }
 
-  img_stack <- get.srtm() %>%
-    ee$Image$addBands(gee_get.pop()) %>%
-    ee$Image$addBands(gee_get.lat()) %>%
-    ee$Image$addBands(gee_get.lon()) %>%
-    ee$Image$addBands(gee_get.lc(date_from, date_to)) %>%
-    ee$Image$addBands(gee_get.dist_coast()) %>%
-    ee$Image$addBands(gee_get.dist_urban()) %>%
-    ee$Image$addBands(gee_get.pm25()) %>%
-    ee$Image$addBands(gee_get.o3(date_from, date_to)) %>%
-    ee$Image$addBands(gee_get.no2(date_from, date_to))
+  if (!is.null(gee_poly)){
+    roi <- gee_poly
+  } else if (!is.null(sf_obj)){
+    roi <- sf_as_ee(sf_obj)$geometry()
+  } else if (!is.null(bbox_obj)){
+    x_min <- bbox_obj[1]
+    y_min <- bbox_obj[2]
+    x_max <- bbox_obj[3]
+    y_max <- bbox_obj[4]
+    roi <- ee$Geometry$Rectangle(list(x_min, y_min, x_max, y_max))
+  } else if (!is.null(coords_vector_list)){
+    roi <- ee$Geometry$Polygon(coords_vector_list)
+  } else {
+    roi <- NULL
+  }
+
+  img_stack <- gee.get_srtm() %>%
+    ee$Image$addBands(gee.get_pop()) %>%
+    ee$Image$addBands(gee.get_lat()) %>%
+    ee$Image$addBands(gee.get_lon()) %>%
+    ee$Image$addBands(gee.get_lc(date_from, date_to)) %>%
+    ee$Image$addBands(gee.get_distcoast()) %>%
+    ee$Image$addBands(gee.get_disturban()) %>%
+    ee$Image$addBands(gee.get_pm25()) %>%
+    ee$Image$addBands(gee.get_o3(date_from, date_to)) %>%
+    ee$Image$addBands(gee.get_no2(date_from, date_to))
 
   img_stack_raster <- ee_as_raster(img_stack,
                                    region=roi,
                                    via='drive',
                                    scale=res)
 
-  writeRaster(img_stack_raster,glue('results/stack_{date_from}_{date_to}.tif'),
-              overwrite=TRUE)
-
+  terra_ras <- terra::rast(img_stack_raster)
+  terra::writeRaster(terra_ras,
+                      glue('results/stack_{date_from}_{date_to}.tif'),
+                      overwrite=TRUE)
   return(img_stack_raster)
 }
 
-# Setting res > 10000 creates problematic NO2 and O3 images
-s <- gee_get.data(date_from = '2022-08-01', date_to = '2022-08-30')
 
-plot(s)
 
 
 
