bbf70aa1b94448573e2e0b8f38786e08cc7fe32f
laurimyllyvirta <laurimyllyvirta@yahoo.com>
2023-10-05 12:45:28 +0300

    pass on the col.index and darken arguments from scale_*_crea_c to makegrad

--- R/crea_theme.R
+++ R/crea_theme.R
@@ -123,12 +123,12 @@ scale_fill_crea_d <- function(palette = "CREA", alpha = 1, col.index=T,...) {
   discrete_scale("fill", palette, makepal(palette, alpha, col.index), ...)
 }
 
-scale_color_crea_c <- function(palette = "CREA", alpha = 1, reverse.order=F, bias=1, ...) {
-  continuous_scale("colour", palette, makegrad(palette, alpha=alpha, reverse.order=reverse.order, bias=bias), ...)
+scale_color_crea_c <- function(palette = "CREA", alpha = 1, reverse.order=F, bias=1, col.index = T, darken = 0, ...) {
+  continuous_scale("colour", palette, makegrad(palette, alpha=alpha, reverse.order=reverse.order, bias=bias, col.index = col.index, darken = darken), ...)
 }
 
-scale_fill_crea_c <- function(palette = "CREA", alpha = 1, reverse.order=F, bias=1, ...) {
-  continuous_scale("fill", palette, makegrad(palette, alpha=alpha, reverse.order=reverse.order, bias=bias), ...)
+scale_fill_crea_c <- function(palette = "CREA", alpha = 1, reverse.order=F, bias=1, col.index = T, darken = 0, ...) {
+  continuous_scale("fill", palette, makegrad(palette, alpha=alpha, reverse.order=reverse.order, bias=bias, col.index = col.index, darken = darken), ...)
 }
 
 scale_y_crea_zero <- function(mult_high=0.1){
