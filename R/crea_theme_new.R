0f76f1403b1c303ac8c6999e388a8c7490a0644d
hubert-crea <hubert@energyandcleanair.org>
2024-09-17 17:50:09 +0000

    -adding font add and showtext to theme

--- R/crea_theme_new.R
+++ R/crea_theme_new.R
@@ -1,17 +1,23 @@
-theme_params <- list(
-  grey1 = "grey20",
-  grey2 = "grey70",
-  grey3 = "grey95",
-  lightblue = pal_crea[["Light.blue"]],
-  darkblue = pal_crea[["Dark.blue"]],
+theme_crea_new <- function(
+  base_family = "Lato",
   fontsize1 = 15,
   fontsize2 = 12,
   fontsize3 = 10,
-  fontsize4 = 8
-)
+  fontsize4 = 8,
+  grey1 = "grey20",
+  grey2 = "grey70",
+  grey3 = "grey95",
+  strip_background = pal_crea[["Blue"]],
+  strip_color = "white",
+  title_color = pal_crea[["Dark.blue"]]
+  ){
 
-theme_crea_new <- function(theme_params=theme_params){
-  theme_minimal(base_family = "Lato", base_size = 14) +  # Adjust size & font
+
+  # Prep environment
+  showtext::showtext_auto()
+  try(sysfonts::font_add_google(base_family, regular = "400", bold = "700"))
+
+  theme_minimal(base_family = base_family, base_size = fontsize2) +  # Adjust size & font
   theme(
 
     # Background and box around the chart
@@ -23,31 +29,31 @@ theme_crea_new <- function(theme_params=theme_params){
     panel.border = element_blank(),
 
     # Grid lines
-    panel.grid.major = element_line(color = theme_params$grey3),  # Lighter gray for major grid lines
+    panel.grid.major = element_line(color = grey3),  # Lighter gray for major grid lines
     panel.grid.minor = element_blank(),
 
     # Titles
-    plot.title = element_text(color = theme_params$darkblue, size = theme_params$fontsize1, face = "bold"),
-    plot.subtitle = element_text(color = theme_params$grey1, size = theme_params$fontsize2, margin = margin(b = 10)),
+    plot.title = element_text(color = title_color, size = fontsize1, face = "bold"),
+    plot.subtitle = element_text(color = grey1, size = fontsize2, margin = margin(b = 10)),
 
     # Axis
-    axis.title = element_text(color = theme_params$grey1, size = theme_params$fontsize3),
-    axis.text = element_text(color = theme_params$grey1, size = theme_params$fontsize4),
+    axis.title = element_text(color = grey1, size = fontsize3),
+    axis.text = element_text(color = grey1, size = fontsize4),
 
     # Facet strips (if you're using facets)
-    strip.background = element_rect(fill = theme_params$lightblue, color = NA),
-    strip.text = element_text(color = "white", size = theme_params$fontsize3, margin = margin(4, 0, 4, 0)),
+    strip.background = element_rect(fill = strip_background, color = NA),
+    strip.text = element_text(color = strip_color, size = fontsize3, margin = margin(4, 0, 4, 0)),
 
     # Legend
     legend.position = "top",  # Legend at the bottom
     legend.direction = "horizontal",  # Spread out in one line
     legend.box = "horizontal",
-    legend.title = element_text(color = theme_params$grey1, size=theme_params$fontsize2),
-    legend.text = element_text(color = theme_params$grey1, size=theme_params$fontsize2),
+    legend.title = element_text(color = grey1, size=fontsize2),
+    legend.text = element_text(color = grey1, size=fontsize2),
     legend.key = element_rect(fill = "white", color = NA),
 
     # Caption
-    plot.caption = element_text(color = theme_params$grey1, size = theme_params$fontsize3, hjust = 0,
+    plot.caption = element_text(color = grey1, size = fontsize3, hjust = 0,
                                 margin = margin(t = 20, r = 0, b = 0, l = 0)),
 
     # Box around the plot area
