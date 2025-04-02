b0ffa6a7a9c0d6a1c7420cb1e0ea50ea38c65e3d
Panda Rushwood <panda@energyandcleanair.org>
2025-04-02 17:24:17 +0100

    Fix missing package namespace references

--- R/crea_theme_new.R
+++ R/crea_theme_new.R
@@ -20,38 +20,38 @@ theme_crea_new <- function(
   showtext::showtext_auto()
   showtext::showtext_opts(dpi = 300)
 
-  theme_minimal(base_family = base_family, base_size = fontsize2) + # Adjust size & font
-    theme(
+  ggplot2::theme_minimal(base_family = base_family, base_size = fontsize2) + # Adjust size & font
+    ggplot2::theme(
 
       # Background and box around the chart
-      plot.background = element_rect(fill = "white", color = NA),
-      panel.background = element_rect(fill = "white", color = NA),
+      plot.background = ggplot2::element_rect(fill = "white", color = NA),
+      panel.background = ggplot2::element_rect(fill = "white", color = NA),
 
 
       # Panel border
-      panel.border = element_rect(color = grey3, fill = NA, linewidth = 1),
+      panel.border = ggplot2::element_rect(color = grey3, fill = NA, linewidth = 1),
 
       # Grid lines
-      panel.grid.major = element_line(color = grey3, linewidth = 0.4), # Lighter gray for major grid lines
-      panel.grid.minor = element_blank(),
+      panel.grid.major = ggplot2::element_line(color = grey3, linewidth = 0.4), # Lighter gray for major grid lines
+      panel.grid.minor = ggplot2::element_blank(),
 
       # Titles
-      plot.title = element_text(color = title_color, size = fontsize1, face = "bold"),
+      plot.title = ggplot2::element_text(color = title_color, size = fontsize1, face = "bold"),
       plot.title.position = "plot",
-      plot.subtitle = element_text(color = grey1, size = fontsize2, margin = margin(b = 15)),
+      plot.subtitle = ggplot2::element_text(color = grey1, size = fontsize2, margin = margin(b = 15)),
 
       # Axis
-      axis.title = element_text(color = grey1, size = fontsize3),
-      axis.text = element_text(color = grey1, size = fontsize3),
-      axis.title.x = element_text(margin = margin(t = 10)),
-      axis.title.y = element_text(margin = margin(r = 10)),
+      axis.title = ggplot2::element_text(color = grey1, size = fontsize3),
+      axis.text = ggplot2::element_text(color = grey1, size = fontsize3),
+      axis.title.x = ggplot2::element_text(margin = margin(t = 10)),
+      axis.title.y = ggplot2::element_text(margin = margin(r = 10)),
 
       # Facet strips (if you're using facets)
-      strip.background = element_rect(fill = strip_background, color = NA),
-      strip.text = element_text(
+      strip.background = ggplot2::element_rect(fill = strip_background, color = NA),
+      strip.text = ggplot2::element_text(
         color = strip_color,
         size = fontsize2,
-        margin = margin(4, 0, 4, 0),
+        margin = ggplot2::margin(4, 0, 4, 0),
         face = "bold",
         hjust = ifelse(strip_align == "center", 0.5, 0)
       ),
@@ -60,23 +60,23 @@ theme_crea_new <- function(
       legend.position = "bottom", # Legend at the bottom
       legend.direction = "horizontal", # Spread out in one line
       legend.box = "horizontal",
-      legend.title = element_text(color = grey1, size = fontsize2),
-      legend.text = element_text(color = grey1, size = fontsize2),
-      legend.key = element_rect(fill = "white", color = NA),
+      legend.title = ggplot2::element_text(color = grey1, size = fontsize2),
+      legend.text = ggplot2::element_text(color = grey1, size = fontsize2),
+      legend.key = ggplot2::element_rect(fill = "white", color = NA),
 
       # Caption
-      plot.caption = element_text(
+      plot.caption = ggplot2::element_text(
         color = grey1, size = fontsize2, hjust = 0,
-        margin = margin(t = 20, r = 0, b = 0, l = 0)
+        margin = ggplot2::margin(t = 20, r = 0, b = 0, l = 0)
       ),
       plot.caption.position = "plot",
 
       # Box around the plot area
-      plot.margin = margin(10, 10, 10, 10), # Add some margin around the plot
-      legend.margin = margin(0, 0, 0, 0),
+      plot.margin = ggplot2::margin(10, 10, 10, 10), # Add some margin around the plot
+      legend.margin = ggplot2::margin(0, 0, 0, 0),
 
       # Add space between facets
       strip.placement = "outside",
-      panel.spacing = unit(0.5, "cm") # Add vertical space between facets
+      panel.spacing = ggplot2::unit(0.5, "cm") # Add vertical space between facets
     )
 }
